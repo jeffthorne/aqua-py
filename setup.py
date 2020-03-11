ac30d202fbee87701e79af48d2c611fc6843cf17
jeffthorne <jthorne@u.washington.edu>
2020-03-11 10:34:01 -0700

    added notifications call

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='aqua',
-      version='0.1.6a',
+      version='0.1.7',
       description='Python 3 client for Aqua\'s CSP Platform',
-      url='https://github.com/',
+      url='https://github.com/jeffthorne/aqua-py',
       author='Jeff Thorne',
       author_email='jthorne@u.washington.edu',
       license='MIT',
