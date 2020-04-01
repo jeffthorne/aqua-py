b5826bd3062caf16db34576cc87c7c16111a234b
jeffthorne <jthorne@u.washington.edu>
2020-04-01 05:27:20 -0700

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='aqua',
-      version='0.1.19',
+      version='0.1.20',
       description='Python 3 client for Aqua\'s CSP Platform',
       url='https://github.com/jeffthorne/aqua-py',
       author='Jeff Thorne',
