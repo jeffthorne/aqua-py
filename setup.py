85cf182abbec41998ed78a80003ed7aa5530e6c7
jeffthorne <jthorne@u.washington.edu>
2020-07-23 09:43:27 -0700

    added lcienses calls back in after merge conflict

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='aqua',
-      version='0.1.25',
+      version='0.1.26',
       description='Python 3 client for Aqua\'s CSP Platform',
       url='https://github.com/jeffthorne/aqua-py',
       author='Jeff Thorne',
